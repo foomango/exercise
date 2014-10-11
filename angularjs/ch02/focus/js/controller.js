49b461b145d65aa966a82cc3bc81b3d9bc1fb23a
Ferry Wang <foomango@gmail.com>
2014-10-11 14:23:05 +0800

    Create focus app.

---
+++ angularjs/ch02/focus/js/controller.js
@@ -0,0 +1,20 @@
+function SomeController($scope) {
+    $scope.message = {text: 'nothing clicked yet'};
+
+    $scope.clickUnfocused = function() {
+        $scope.message.text = 'unfocused button clicked';
+    }
+
+    $scope.clickFocused = function() {
+        $scope.message.text = 'focuse button clicked';
+    }
+}
+
+var appModule = angular.module('app', []);
+appModule.directive('ngbkFocus', function() {
+    return {
+        link: function(scope, element, attrs, controller) {
+            element[0].focus();
+        }
+    };
+});
